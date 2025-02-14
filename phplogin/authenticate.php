e63d8574630df5489bdf88be6ffa25d45bf221f8
brage-grrgrr <brage.w.b@gmail.com>
2025-02-14 12:00:25 +0000

    kopierer koden til hanna

--- phplogin/authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Change this to your connection info.
-$host = '172.20.128.61';
-$user = 'brage';
-$pass = 'drlig7i0';
-$db = 'Login_side';
-// Try and connect using the info above.
-$con = mysqli_connect($host, $user, $pass, $db);
